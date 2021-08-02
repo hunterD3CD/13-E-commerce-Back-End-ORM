ad40995ebdce4959f67d4aaa1932abe0f79b1c60
hunterD3CD <hunterd3c@gmail.com>
2021-08-01 17:53:58 -0700

    debug with TA, foreign key constrain fails

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -139,26 +139,57 @@ router.put("/:id", (req, res) => {
     });
 });
 
+// router.delete("/:id", (req, res) => {
+//   // delete one product by its `id` value
+//   Product.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((productData) => {
+//       if (!productData) {
+//         res
+//           .status(404)
+//           .json({ message: "can't find the product with this id" });
+//         return;
+//       }
+//       res.json(productData);
+//     })
+//     .catch((err) => {
+//       // console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   Product.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((products) => {
+//       console.log(products);
+//       res.json(products);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
+
 router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
   Product.destroy({
     where: {
       id: req.params.id,
     },
   })
-    .then((productData) => {
-      if (!productData) {
-        res
-          .status(404)
-          .json({ message: "can't find the product with this id" });
-        return;
-      }
-      res.json(productData);
+    .then((products) => {
+      console.log(products);
+      res.json(products);
     })
     .catch((err) => {
-      // console.log(err);
-      res.status(500).json(err);
+      console.log(err);
+      res.status(400).json(err);
     });
 });
-
 module.exports = router;
